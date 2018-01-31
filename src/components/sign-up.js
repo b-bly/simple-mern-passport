81adb92844122f9c1a93e74763bcf2a182120796
Brendt Bly <brendt.bly@outlook.com>
2018-01-31 15:24:47 -0600

    styling to signup

--- src/components/sign-up.js
+++ src/components/sign-up.js
@@ -9,7 +9,7 @@ class SignupForm extends Component {
 			username: '',
 			password: '',
 			confirmPassword: '',
-			
+
 		}
 		this.handleSubmit = this.handleSubmit.bind(this)
 		this.handleChange = this.handleChange.bind(this)
@@ -21,48 +21,72 @@ class SignupForm extends Component {
 	}
 	handleSubmit(event) {
 		event.preventDefault()
-        console.log('sign-up-form, username: ');
-        console.log(this.state.username);
+		console.log('sign-up-form, username: ');
+		console.log(this.state.username);
 		//request to server here
 		axios.post('/', {
 			username: this.state.username,
 			password: this.state.password
 		})
-		.then(response => {
-			console.log(response)
-			if (!response.data.errmsg) {
-				console.log('successful signup')
-				this.setState({
-					redirectTo: '/login'
-				})
-			} else {
-				console.log('username already taken')
-			}
-		})
+			.then(response => {
+				console.log(response)
+				if (!response.data.errmsg) {
+					console.log('successful signup')
+					this.setState({
+						redirectTo: '/login'
+					})
+				} else {
+					console.log('username already taken')
+				}
+			})
 
 	}
 	render() {
 
 		return (
 			<div className="SignupForm">
-				<h1>Signup form</h1>
-				<label htmlFor="username">Username: </label>
-				<input
-					type="text"
-					name="username"
-					value={this.state.username}
-					onChange={this.handleChange}
-				/>
-				<label htmlFor="password">Password: </label>
-				<input
-					type="password"
-					name="password"
-					value={this.state.password}
-					onChange={this.handleChange}
-				/>
-				
-				<button onClick={this.handleSubmit}>Sign up</button>
+
+				<form className="form-horizontal">
+					<div className="form-group">
+						<div className="col-1 col-ml-auto">
+							<label className="form-label" htmlFor="username">Username</label>
+						</div>
+						<div className="col-3 col-mr-auto">
+							<input className="form-input"
+								type="text"
+								id="input-example-1"
+								placeholder="Name"
+								value={this.state.username}
+								onChange={this.handleChange}
+							/>
+						</div>
+					</div>
+					<div className="form-group">
+						<div className="col-1 col-ml-auto">
+							<label className="form-label" htmlFor="password">Password: </label>
+						</div>
+						<div className="col-3 col-mr-auto">
+							<input className="form-input"
+								placeholder="password"
+								type="password"
+								name="password"
+								value={this.state.password}
+								onChange={this.handleChange}
+							/>
+						</div>
+
+					</div>
+
+					<div className="form-group ">
+						<div className="col-7"></div>
+						
+							<button className="btn btn-primary col-1 col-mr-auto" onClick={this.handleSubmit}>Sign up</button>
+						
+
+					</div>
+				</form>
 			</div>
+
 		)
 	}
 }
