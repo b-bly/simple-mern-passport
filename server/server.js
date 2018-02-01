d212050e67dede2a0b00b0a6ed8348ea8a8bf07c
Brendt Bly <brendt.bly@outlook.com>
2018-01-31 19:49:11 -0600

    express sessions added and print to terminal

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const session = require('express-session')
 const dbConnection = require('./database') 
 const app = express()
 const PORT = 8080
@@ -15,6 +16,17 @@ app.use(
 	})
 )
 app.use(bodyParser.json())
+
+//sessions
+app.use(
+	session({
+		secret: 'fraggle-rock', //pick a random string to make the hash that is generated secure
+	})
+)
+app.use( (req, res, next) => {
+	console.log('req.session', req.session);
+	return next();
+  });
 
 // routes
 app.use('/user', user)
