ed9437fce620ac84dfca1121c8f3e2a19cfcb606
Brendt Bly <brendt.bly@outlook.com>
2018-02-04 12:08:37 -0600

    commented index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom' //don't need to specify localhost url in axios http address
+import { BrowserRouter } from 'react-router-dom' //may help sessions store cookie with session id from server
+//add proxy   "proxy": "http://localhost:8080" to package.json
+//so that you don't need to specify this in every axios request
 
 //style
 import 'spectre.css/dist/spectre.min.css';
