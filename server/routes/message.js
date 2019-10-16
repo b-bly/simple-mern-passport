eea25aff45b7676f927bdf6e6f7b917d9d040a33
Brady Douthit <brady.douthit99@gmail.com>
2019-10-16 11:52:08 -0400

    posting a message now updates the channel with the message ID

--- server/routes/message.js
+++ server/routes/message.js
@@ -1,12 +1,22 @@
 const express = require('express')
 const router = express.Router()
 const Message = require('../database/models/message')
+const Channel = require('../database/models/channel')
 
 router.post('/messages/:channelID', (req, res) => {
-    console.log('req.body: ')
-    console.log(req.body)
-    res.json(req.body)
-    //Channel.create({"ChannelName": req.body.ChannelName, "Messages": req.body.Messages, "Users": req.body.Users})
+
+   return Message.create({
+        "channelName": req.body.channelName, 
+        "sender": req.body.sender, 
+        "channelID": req.body.channelID, 
+        "messageBody": req.body.messageBody
+    }).then(function(message) {
+        console.log('message: ')
+        console.log(message)
+        res.json(message)
+        return Channel.findOneAndUpdate({_id: message.channelID}, {$push: {messages: message._id}}, {new: true})
+    })
+
 })
 
 
