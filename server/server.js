044919f556ee1cbfd6d69e2a58e53a3f176ae189
Charles Mercer <c.e.mercer15@gmail.com>
2019-10-26 09:33:37 -0400

    changes to server file and package.json

--- server/server.js
+++ server/server.js
@@ -40,7 +40,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+    app.use(express.static(__dirname + "client/build"));
 }
 
 //Socket Connection
