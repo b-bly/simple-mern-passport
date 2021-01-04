36a974229d785188db746486eec45c719fdd1135
Brady Douthit <brady.douthit99@gmail.com>
2021-01-04 12:23:49 -0500

    add a catchall route

--- server.js
+++ server.js
@@ -14,7 +14,13 @@ const MessageModel = require('./models/message')
 const channel = require('./routes/channel')
 const message = require('./routes/message')
 const mongoose = require('mongoose')
+const path = require('path');
 
+if (process.env.NODE_ENV === 'production') {
+	app.get("*", (req, res) => {
+		res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+	});	
+}
 // Starting Server 
 const server = app.listen(PORT, () => {
 	console.log(`App listening on PORT: ${PORT}`)
